7d885d0fb9938ec2cb89b14609a55645c1b9d312
Integral ZZZ <chemistgmo@gmail.com>
2022-06-04 14:12:57 +0300

    Ц

---
+++ фвавы.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Integral on 04.06.2022.
+//
+
+#include "фвавы.h"
---
+++ фвавы.h
@@ -0,0 +1,16 @@
+//
+// Created by Integral on 04.06.2022.
+//
+
+#ifndef RAL_ФВАВЫ_H
+#define RAL_ФВАВЫ_H
+
+
+
+class фвавы {
+
+};
+
+
+
+#endif //RAL_ФВАВЫ_H
